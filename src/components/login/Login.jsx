78af6f3bb1edd25c293867ee9dacd9cd378f9779
Sagar <sagarpatidar2002@gmail.com>
2024-06-17 15:06:20 +0530

    Add change handler in login/signup form

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,22 +1,40 @@
-
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./Login.css";
+
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (username && password) {
+      console.log('Form Submitted:', { username, password });
+      // Perform login API call
+    } else {
+      alert('Please fill in both fields.');
+    }
+  };
+
   return (
     <>
       <div className="lo-container">
         <div className="lo-header">
-          <img src="./images/title/6-removebg-preview (1).png" />
+          <img src="./images/title/6-removebg-preview (1).png" alt="Logo" />
           <h1>WELCOME TO YOUTHSPIRE</h1>
         </div>
         <div className="lo-form-container">
           <div className="login-box">
-            <form action="#" id="login-form">
+            <form id="login-form" onSubmit={handleSubmit}>
               <div className="input-group">
                 <input
                   type="text"
                   id="username"
                   name="username"
                   placeholder="Phone number, username or email"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
                   required
                 />
               </div>
@@ -26,39 +44,41 @@ function Login() {
                   id="password"
                   name="password"
                   placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                 />
               </div>
               <button id="login-btn" type="submit">Log in</button>
             </form>
             <div className="button-group">
-              <a className="Google" href="#">
+              <Link className="Google" to="#">
                 Log in with Google
-              </a>
-              <a className="x" href="#">
+              </Link>
+              <Link className="x" to="#">
                 Log in with X
-              </a>
-              <a className="Facebook" href="#">
+              </Link>
+              <Link className="Facebook" to="#">
                 Log in with Facebook
-              </a>
+              </Link>
             </div>
           </div>
           <div className="links">
-            <a href="#">Forgot password?</a>
-            <a href="#">Dont have an account? Sign up</a>
+            <Link href="#">Forgot password?</Link>
+            <Link to="/signup">Don&apos;t have an account? Sign up</Link>
           </div>
         </div>
         <div className="footer">
-          <img src="./images/title/6-removebg-preview (1).png" />
-          <img src="./images/title/7-removebg-preview (1).png" />
-          <img src="./images/title/8-removebg-preview (1).png" />
-          <img src="./images/title/9-removebg-preview (1).png" />
-          <img src="./images/title/10-removebg-preview (1).png" />
-          <img src="./images/title/11-removebg-preview (1).png" />
-          <img src="./images/title/12-removebg-preview (1).png" />
-          <img src="./images/title/13-removebg-preview (1).png" />
-          <img src="./images/title/14-removebg-preview (1).png" />
-          <img src="./images/title/15-removebg-preview (1).png" />
+          <img src="./images/title/6-removebg-preview (1).png" alt="Logo 1" />
+          <img src="./images/title/7-removebg-preview (1).png" alt="Logo 2" />
+          <img src="./images/title/8-removebg-preview (1).png" alt="Logo 3" />
+          <img src="./images/title/9-removebg-preview (1).png" alt="Logo 4" />
+          <img src="./images/title/10-removebg-preview (1).png" alt="Logo 5" />
+          <img src="./images/title/11-removebg-preview (1).png" alt="Logo 6" />
+          <img src="./images/title/12-removebg-preview (1).png" alt="Logo 7" />
+          <img src="./images/title/13-removebg-preview (1).png" alt="Logo 8" />
+          <img src="./images/title/14-removebg-preview (1).png" alt="Logo 9" />
+          <img src="./images/title/15-removebg-preview (1).png" alt="Logo 10" />
         </div>
       </div>
     </>
--- src/components/sign up/Signup.jsx
+++ src/components/sign up/Signup.jsx
@@ -1,41 +1,71 @@
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Signup.css";
+
 function Signup() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    username: '',
+    password: '',
+    termsAccepted: false,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === 'checkbox' ? checked : value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.termsAccepted) {
+      console.log('Form Submitted:', formData);
+      // Perform signup API call
+    } else {
+      alert('Please accept the terms and conditions.');
+    }
+  };
+
   const inlinestyle = {
     height: "18px",
-    Width: "18px",
+    width: "18px",
   };
 
   return (
     <>
       <div className="lo-container">
         <div className="lo-header">
-          <img src="./images/title/6-removebg-preview (1).png" />
+          <img src="./images/title/6-removebg-preview (1).png" alt="Logo" />
           <h1>WELCOME TO YOUTHSPIRE</h1>
         </div>
         <div className="lo-form-container">
           <div className="box">
-            <form action="#">
+            <form onSubmit={handleSubmit}>
               <div className="wrapper">
                 <div className="first-2">
                   <div className="input-group">
-                    <label>Name</label>
+                    <label htmlFor="name">Name</label>
                     <input
                       type="text"
-                      id="username"
-                      name="Name"
-                      placeholder=""
+                      id="name"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleChange}
                       required
                     />
                   </div>
 
                   <div className="input-group">
-                    <label>Email</label>
+                    <label htmlFor="email">Email</label>
                     <input
-                      type="text"
-                      id="Email"
-                      name="Name"
-                      placeholder=""
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
                       required
                     />
                   </div>
@@ -43,23 +73,25 @@ function Signup() {
 
                 <div className="last-2">
                   <div className="input-group">
-                    <label>Username</label>
+                    <label htmlFor="username">Username</label>
                     <input
                       type="text"
                       id="username"
                       name="username"
-                      placeholder=""
+                      value={formData.username}
+                      onChange={handleChange}
                       required
                     />
                   </div>
 
                   <div className="input-group">
-                    <label>Create password</label>
+                    <label htmlFor="password">Create password</label>
                     <input
                       type="password"
                       id="password"
                       name="password"
-                      placeholder=""
+                      value={formData.password}
+                      onChange={handleChange}
                       required
                     />
                   </div>
@@ -71,8 +103,10 @@ function Signup() {
                   <input
                     type="checkbox"
                     id="terms"
+                    name="termsAccepted"
+                    checked={formData.termsAccepted}
+                    onChange={handleChange}
                     style={inlinestyle}
-                    name=""
                   />
                   <label htmlFor="terms">Accept terms and conditions</label>
                 </div>
@@ -81,20 +115,20 @@ function Signup() {
             </form>
           </div>
           <div className="links">
-            <a href="#">Already have an account? log in</a>
+            <Link to="/login">Already have an account? Log in</Link>
           </div>
         </div>
         <div className="footer">
-          <img src="./images/title/6-removebg-preview (1).png" />
-          <img src="./images/title/7-removebg-preview (1).png" />
-          <img src="./images/title/8-removebg-preview (1).png" />
-          <img src="./images/title/9-removebg-preview (1).png" />
-          <img src="./images/title/10-removebg-preview (1).png" />
-          <img src="./images/title/11-removebg-preview (1).png" />
-          <img src="./images/title/12-removebg-preview (1).png" />
-          <img src="./images/title/13-removebg-preview (1).png" />
-          <img src="./images/title/14-removebg-preview (1).png" />
-          <img src="./images/title/15-removebg-preview (1).png" />
+          <img src="./images/title/6-removebg-preview (1).png" alt="Logo 1" />
+          <img src="./images/title/7-removebg-preview (1).png" alt="Logo 2" />
+          <img src="./images/title/8-removebg-preview (1).png" alt="Logo 3" />
+          <img src="./images/title/9-removebg-preview (1).png" alt="Logo 4" />
+          <img src="./images/title/10-removebg-preview (1).png" alt="Logo 5" />
+          <img src="./images/title/11-removebg-preview (1).png" alt="Logo 6" />
+          <img src="./images/title/12-removebg-preview (1).png" alt="Logo 7" />
+          <img src="./images/title/13-removebg-preview (1).png" alt="Logo 8" />
+          <img src="./images/title/14-removebg-preview (1).png" alt="Logo 9" />
+          <img src="./images/title/15-removebg-preview (1).png" alt="Logo 10" />
         </div>
       </div>
     </>
